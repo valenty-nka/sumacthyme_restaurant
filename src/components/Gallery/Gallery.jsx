dbfb3998c19119125834695de5958cd15cd802d3
VALENTYNA NECHYPORENKO <valentynanechyporenko@VALENTYNAs-MacBook-Pro.local>
2025-10-02 20:20:14 -0230

    Add new images to gallery. Create gallery folder. Update gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,16 +4,26 @@ import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import gallery1 from "../../assets/images/gallery1.jpg";
-import gallery2 from "../../assets/images/gallery2.jpg";
-import gallery3 from "../../assets/images/gallery3.jpg";
-import gallery4 from "../../assets/images/gallery4.jpg";
-import gallery5 from "../../assets/images/gallery5.jpg";
-import gallery6 from "../../assets/images/gallery6.jpg";
-import gallery7 from "../../assets/images/gallery7.jpeg";
+import gallery0 from "../../assets/images/gallery/gallery0.jpg";
+import gallery1 from "../../assets/images/gallery/gallery1.jpg";
+import gallery2 from "../../assets/images/gallery/gallery2.jpg";
+import gallery3 from "../../assets/images/gallery/gallery3.jpg";
+import gallery4 from "../../assets/images/gallery/gallery4.jpg";
+import gallery5 from "../../assets/images/gallery/gallery5.jpg";
+import gallery6 from "../../assets/images/gallery/gallery6.jpg";
+import gallery7 from "../../assets/images/gallery/gallery7.jpeg";
+import gallery8 from "../../assets/images/gallery/gallery8.jpg";
+import gallery9 from "../../assets/images/gallery/gallery9.jpg";
+import gallery10 from "../../assets/images/gallery/gallery10.jpg";
+import gallery11 from "../../assets/images/gallery/gallery11.jpg";
+import gallery12 from "../../assets/images/gallery/gallery12.jpg";
+import gallery13 from "../../assets/images/gallery/gallery13.jpg";
+import gallery14 from "../../assets/images/gallery/gallery14.jpg";
+import gallery15 from "../../assets/images/gallery/gallery15.jpg";
 
 
-const photos = [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7];
+
+const photos = [gallery0, gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9, gallery10, gallery11, gallery12, gallery13, gallery14, gallery15];
 
 export default function Gallery() {
   return (
